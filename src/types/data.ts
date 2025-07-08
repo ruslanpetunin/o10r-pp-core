5d624d109ee05d66c6e77815c3efe89c43f522d8
Ruslan <r.petunin@it.ecommpay.com>
2025-07-08 17:05:07 +0300

    Payment methods

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,14 +1,37 @@
+export interface ProjectSettings {
+  methods: PaymentMethodData[];
+}
+
 export interface InitData {
   amount: number;
   currency: string;
   project_hash: string;
 }
 
-export interface ProjectSettings {
-  methods: Method[];
-}
-
-export interface Method {
+export interface PaymentMethodData {
   code: string,
   icon: string,
+  fields: PaymentMethodField[]
+}
+
+export enum PaymentMethodFieldType {
+  NUMBER = 'number',
+  TEXT = 'text',
+  EMAIL = 'email',
+  TEL = 'tel',
+  PASSWORD = 'password',
+}
+
+export interface PaymentMethodFieldValidator {
+  name: string,
+  options?: Record<string, unknown>,
+}
+
+export type PaymentMethodField = PaymentMethodFieldBase;
+
+export interface PaymentMethodFieldBase {
+  name: string,
+  type: PaymentMethodFieldType,
+  autocomplete?: string,
+  validators: PaymentMethodFieldValidator[],
 }
