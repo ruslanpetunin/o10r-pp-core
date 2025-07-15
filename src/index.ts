718d25d421018c38224fe5ee5dea675ca74c67ea
Ruslan <r.petunin@it.ecommpay.com>
2025-07-15 10:49:26 +0300

    Removing payment methods

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ export { default as useEventManager } from './composables/useEventManager';
 export { default as useJwtToken } from './composables/useJwtToken';
 export { default as useApi } from './composables/useApi';
 export { default as useHttp } from './composables/useHttp';
-export { default as useProjectSettings } from './composables/useProjectSettings';
 export { default as useTranslator } from './composables/useTranslator';
 
 export * from './types/event';
 export * from './types/data';
 export * from './types/api';
 export * from './types/http';
-export * from './types/project';
 export * from './types/translator';
