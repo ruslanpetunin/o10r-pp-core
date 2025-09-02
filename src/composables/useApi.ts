486723180579ea50043d327d5b04bbd8a5332416
Ruslan <r.petunin@it.ecommpay.com>
2025-09-02 11:06:25 +0300

    Clarification

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,6 +1,5 @@
 import type { Api, ApiResponse } from '../types/api';
 import type { PaymentStatusData, SavedCard, SessionData, TranslationData } from '../types/data'
-// import { PaymentStatus } from '../types/data';
 import type { Language } from '../types/translator';
 import useHttp from './useHttp';
 import { HttpMethod } from "./../types/http";
@@ -16,7 +15,6 @@ export default function(host: string): Api {
     return request<TranslationData>(`/translations/${language}.json`);
   }
 
-  // let attempts = 0;
   async function getPaymentStatus(sid: string): Promise<PaymentStatusData> {
     return request<ApiResponse<PaymentStatusData>>(`${host}/v1/status?client_secret=${sid}`)
       .then(
@@ -26,119 +24,6 @@ export default function(host: string): Api {
           return data;
         }
       );
-    // console.log(`Getting payment status via sid: ${sid}`);
-    //
-    // attempts += 1;
-    //
-    // const kindaRequest = new Promise<PaymentStatusData>(
-    //   (resolve) => {
-    //     setTimeout(() => {
-    //       if (attempts === 1) {
-    //         resolve({
-    //           status: PaymentStatus.NOT_STARTED,
-    //         });
-    //       } else if (attempts < 5) {
-    //         resolve({
-    //           status: PaymentStatus.PENDING,
-    //           payment_method_code: 'card'
-    //         });
-    //       } else if (attempts < 10) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_3DS_RESULT,
-    //           account: {
-    //             number: '4242******424242',
-    //             expiry_month: '11',
-    //             expiry_year: '33',
-    //             card_holder: 'SOME CARDHOLDER'
-    //           },
-    //           threeds: {
-    //             is_cascading: true,
-    //             redirect: {
-    //               url: 'https://example.com/3ds-iframe',
-    //               params: {
-    //                 md: 'md-data',
-    //                 pa_req: 'pa_req-data',
-    //                 term_url: 'https://example.com/term-url'
-    //               }
-    //             }
-    //           },
-    //           payment_method_code: 'card'
-    //         });
-    //       }  else if (attempts < 15) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_CLARIFICATION,
-    //           clarification_fields: [
-    //             { name: 'first_name', type: 'text', validation: { required: [] } },
-    //             { name: 'last_name', type: 'text', validation: { required: [] } },
-    //             { name: 'email', type: 'text' },
-    //           ],
-    //           payment_method_code: 'card'
-    //         });
-    //       }  else if (attempts < 15) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_CUSTOMER_ACTION,
-    //           payment_method_code: 'card'
-    //         });
-    //       }  else if (attempts < 10) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_REDIRECT,
-    //           redirect: {
-    //             url: 'https://example.com/aps',
-    //             method: 'GET'
-    //           },
-    //           payment_method_code: 'card'
-    //         });
-    //       }  else if (attempts < 15) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_3DS_RESULT,
-    //           account: {
-    //             number: '4242******424242',
-    //             expiry_month: '11',
-    //             expiry_year: '33',
-    //             card_holder: 'SOME CARDHOLDER'
-    //           },
-    //           threeds: {
-    //             iframe: {
-    //               url: 'https://example.com/3ds-iframe',
-    //               params: {
-    //                 threeDSMethodData: 'some-data',
-    //                 '3DSMethodData': 'some-data'
-    //               }
-    //             },
-    //           },
-    //           payment_method_code: 'card'
-    //         });
-    //       }  else if (attempts < 20) {
-    //         resolve({
-    //           status: PaymentStatus.AWAITING_3DS_RESULT,
-    //           account: {
-    //             number: '4242******424242',
-    //             expiry_month: '11',
-    //             expiry_year: '33',
-    //             card_holder: 'SOME CARDHOLDER'
-    //           },
-    //           threeds: {
-    //             redirect: {
-    //               url: 'https://example.com/3ds-redirect',
-    //               params: {
-    //                 threeDSSessionData: 'session-data',
-    //                 creq: 'creq-data'
-    //               }
-    //             },
-    //           },
-    //           payment_method_code: 'card'
-    //         });
-    //       } else {
-    //         resolve({
-    //           status: PaymentStatus.SUCCESS,
-    //           payment_method_code: 'card'
-    //         });
-    //       }
-    //     }, 1000);
-    //   }
-    // );
-    //
-    // return await kindaRequest;
   }
 
   async function removeSavedCard(sid: string, cardId: number): Promise<void> {
@@ -196,7 +81,15 @@ export default function(host: string): Api {
   }
 
   async function clarify(sid: string, data: Record<string, unknown>): Promise<void> {
-    console.log(`Clarify request is not implemented yet. sid: ${sid}, Data:`, data);
+    await request(
+      `${host}/v1/clarification?client_secret=${sid}`,
+      HttpMethod.POST,
+      {
+        body: {
+          fields: data
+        }
+      }
+    );
   }
 
   return {
