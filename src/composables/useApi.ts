f13493dfaf66a4c7b5c27515b1c76d7aec51e1d6
Ruslan <r.petunin@it.ecommpay.com>
2025-09-05 17:27:07 +0300

    Saved cards getting

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,5 +1,5 @@
 import type { Api, ApiResponse } from '../types/api';
-import type { PaymentStatusData, SavedCard, SessionData, TranslationData } from '../types/data'
+import type { PaymentStatusData, SavedCard, SessionData, TranslationData } from '../types/data';
 import type { Language } from '../types/translator';
 import useHttp from './useHttp';
 import { HttpMethod } from "./../types/http";
@@ -33,38 +33,7 @@ export default function(host: string): Api {
   }
 
   async function getSavedCards(sid: string): Promise<SavedCard[]> {
-    console.log(`Getting saved cards via sid: ${sid}`);
-
-    return await new Promise(
-      (resolve) => {
-        setTimeout(
-          () => resolve([
-            {
-              id: 1,
-              last_four_digits: '4211',
-              type: 'visa',
-              expiry_month: '11',
-              expiry_year: '25'
-            },
-            {
-              id: 2,
-              last_four_digits: '1233',
-              type: 'mastercard',
-              expiry_month: '09',
-              expiry_year: '26'
-            },
-            {
-              id: 3,
-              last_four_digits: '3781',
-              type: 'visa',
-              expiry_month: '01',
-              expiry_year: '25'
-            },
-          ]),
-          5000
-        )
-      }
-    );
+    return request<ApiResponse<SavedCard[]>>(`${host}/v1/customer/card?client_secret=${sid}`).then(({ data }) => data);
   }
 
   async function pay(sid: string, paymentMethodCode: string, data: Record<string, unknown>): Promise<void> {
